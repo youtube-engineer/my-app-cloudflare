d0e5472dbf284752a2a65c61c3117e83dd9504d6
youtube-engineer <chatgptinjapan@gmail.com>
2025-01-20 10:34:55 +0000

    Homeコンポーネントでサーバーアクションが実行できない

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import Form from '@/components/form';
-export const runtime = "edge";
+'use client'
+
+import { useState } from 'react';
+import { createSharedText } from '../actions/text-share';
 
 export default function Home() {
+  const [text, setText] = useState('');
+  const setData = { text }
+
+
+  console.log("createSharedText実行")
 
   return (
-    <Form />
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      {/* <h1 className="text-4xl font-bold mb-6 text-blue-600">{result.text}</h1> */}
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="シェアしたいテキストを入力してください"
+        className="placeholder-gray-300 text-black w-full max-w-2xl p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent mb-4"
+        rows={5}
+      />
+      <button onClick={async () => await createSharedText()} className="bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-all duration-200">
+        シェアする
+      </button>
+    </div>
   );
 }
