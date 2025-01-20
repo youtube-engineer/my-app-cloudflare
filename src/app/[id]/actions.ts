c8fd18783180087341e44a0fefeb36881dbbde50
youtube-engineer <chatgptinjapan@gmail.com>
2025-01-20 10:03:12 +0000

    [id]内にserver actionを実装

--- src/app/[id]/actions.ts
+++ src/app/[id]/actions.ts
@@ -1,22 +1,7 @@
-"use server";
+'use server';
 
-export async function addition(a: number, b: number): Promise<number> {
-    "use server";
-    return a + b;
+export async function createSharedText() {
+    console.log("createSharedText実行")
 }
 
-export async function getIP(): Promise<string> {
-    "use server";
-    const url = "https://api.ipify.org?format=json"; // IP取得サービス
-    try {
-        const response = await fetch(url); // リクエストを送信
-        if (!response.ok) {
-            throw new Error(`HTTPエラー: ${response.status}`);
-        }
-        const data = await response.json(); // JSONレスポンスを解析
-        return data.ip; // IPアドレスを返す
-    } catch (error) {
-        console.error("IP取得中にエラーが発生しました:", error);
-        throw new Error("IPアドレスの取得に失敗しました。");
-    }
-}
+
--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,28 +1,30 @@
-import Form from "./form";
+// import Form from "./form";
 
-export const runtime = "edge";
+import { createSharedText } from "./actions";
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-    return (
-        <main>
-            <h1>
-                Page {id}
-            </h1>
-            <Form />
-        </main>
-    );
-}
 // export const runtime = "edge";
 
-// export default async function ShareText({ params }: { params: Promise<any> }) {
-//     const { id } = await params
-
+// export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+//     const { id } = await params;
 //     return (
-//         <h1 className="p-16">
-//             {id}
-//         </h1>
+//         <main>
+//             <h1>
+//                 Page {id}
+//             </h1>
+//             <Form />
+//         </main>
 //     );
 // }
+export const runtime = "edge";
+
+export default async function ShareText({ params }: { params: Promise<any> }) {
+    const { id } = await params
+    await createSharedText()
+    return (
+        <h1 className="p-16">
+            {id}
+        </h1>
+    );
+}
 
 
